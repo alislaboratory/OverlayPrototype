f4dd74c60b9c58bb2f56d49b08a7337d817c5235
Ali A <ali@ashrafy.au>
2025-07-24 21:54:38 +1000

    Update test_display.py

--- test_display.py
+++ test_display.py
@@ -2,6 +2,6 @@ from transparent_display import TransparentDisplay
 from time import sleep
 
 display = TransparentDisplay()
-points = ((1,1), (1,20) (20,1), (20,20))
+points = ((1,1), (1,20), (20,1), (20,20))
 display.draw_bounding_box(points)
 sleep(5)
