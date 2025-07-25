c7109ed24c331bf7160513354cdba55bfdd3b992
Ali A <ali@ashrafy.au>
2025-07-25 17:03:30 +1000

    Update test_display.py

--- test_display.py
+++ test_display.py
@@ -4,10 +4,13 @@ from time import sleep
 display = TransparentDisplay()
 points = ((1,1), (20,1), (1,20), (20,20))
 display.draw_bounding_box(points)
-sleep(20)
+sleep(1)
 display.clear()
 display.draw_midpoint(points)
 sleep(1)
+display.clear()
+display.draw_text(0,0,0)
+sleep(10)
 #######
 #     #
 #     #
