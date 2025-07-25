626fde38abab3f170b12d5e4558c5e50cd42e0ba
Ali A <ali@ashrafy.au>
2025-07-25 19:21:29 +1000

    Update test_display.py

--- test_display.py
+++ test_display.py
@@ -1,16 +1,21 @@
 from transparent_display import TransparentDisplay
 from time import sleep
+import random
 
 display = TransparentDisplay()
 points = ((1,1), (20,1), (1,20), (20,20))
 display.draw_bounding_box(points)
-sleep(1)
-display.clear()
-display.draw_midpoint(points)
-sleep(1)
-display.clear()
-display.draw_text(0,0,0)
-sleep(60)
+sleep(5)
+def rx():
+    return random.randint(0,127)
+def ry():
+    return random.randint(0,64)
+while True:
+    points = ((rx(), ry()), (rx(), ry()), (rx(), ry()), (rx(), ry()))
+    display.draw_bounding_box(points)
+    sleep(0.01)
+
+
 #######
 #     #
 #     #
