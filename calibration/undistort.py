e3ed68069233d887b90cca678d788f51c82d7c96
Ali A <ali@ashrafy.au>
2025-07-26 18:48:02 +1000

    Update undistort.py

--- calibration/undistort.py
+++ calibration/undistort.py
@@ -5,7 +5,7 @@ from picamera2 import Picamera2
 
 # ?? Configuration ??
 CALIB_FILE    = "camera_calibration.yaml"
-PREVIEW_SIZE  = (1280, 720)
+PREVIEW_SIZE  = (800, 600)
 FLIP_180      = True    # set False if camera is upright
 WINDOW_NAME   = "Live Undistorted Preview (q to quit)"
 
