04dc6f5ce46a07042c93488d73b0c41229d1e7f1
Ali A <ali@ashrafy.au>
2025-09-30 20:03:20 +1000

    working on threading

--- V0.5/overlay.py
+++ V0.5/overlay.py
@@ -47,9 +47,9 @@ def open_camera():
     proc = subprocess.Popen(["rpicam-hello", "--camera", "1", "--vflip", "--timeout", "0"], check=True)
     
 
-t = threading.Thread(target=open_camera, args=(["python", "-V"],), daemon=True)
+t = threading.Thread(target=open_camera, args=(["python", "-V"]), daemon=True)
 t.start()
-print("OverlayPT: Camera thread started")
+print("OverlayPT: Camera thread")
 
 print("Press 'q' to quit...")
 
